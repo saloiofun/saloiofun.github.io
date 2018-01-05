ac166f931884332d37351efa28258b02a739681d
Sandro Wong <saloiofun@gmail.com>
2018-01-04 17:52:23 -0800

    create MERN

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+const PORT = process.env.PORT || 3001
+const app = express()
+const mongoose = require('mongoose')
+
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }))
+// Configure body parser for AJAX requests
+app.use(bodyParser.json())
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
+app.use(routes)
+
+// Set up promises with mongoose
+mongoose.Promise = global.Promise
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/portfolio',
+  {
+    useMongoClient: true
+  }
+)
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> Server now on port ${PORT}!`)
+})
